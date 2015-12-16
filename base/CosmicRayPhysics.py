c10e6150cf6ae8929e7d7d6f3af89bef12ca2ce5
zimmer <zimmer@fysik.su.se>
2015-12-16 14:32:02 +0100

    prototype - should almost work.

--- base/CosmicRayPhysics.py
+++ base/CosmicRayPhysics.py
@@ -4,9 +4,26 @@ Created on Dec 16, 2015
 @author: zimmer
 @brief:  base class for CR model definitions. 
 '''
-from __builtin__ import None
+import subprocess, os, pyfits
+
+from skymaps import SkyDir
+
 from uw.like.Models import *
-from uw.like.SpatialModels import *
+from uw.like.SpatialModels import InterpProfile
+
+from dsphs.utils.par2cmd import par2cmd
+from dsphs.utils.tools import yaml_load
+
+class TabulatedProfile(InterpProfile):
+    def __init__(self,datfile,center=None,kind='cubic'):
+        r_in_degrees, pdf = np.loadtxt(datfile,unpack=True)
+        kwargs = {}
+        kwargs['r_in_degrees']=r_in_degrees
+        kwargs['pdf']=pdf
+        kwargs['kind']=kind
+        kwargs['center']=center
+        super(TabulatedProfile,self).__init__(**kwargs)
+
 
 class CRModel(object):
     '''
@@ -17,30 +34,69 @@ class CRModel(object):
         '''
         Constructor
         '''
-        self.spectrum = None
-        self.spatialModel = None
-        self.name = None
+        self.spectrum           = FileFunction(normalization=1, file=params['spectrum'])
+        self.center             = SkyDir(194.9531, 27.9807,SkyDir.EQUATORIAL)
+        self.spatialModel       = TabulatedProfile(params['profile'],center=self.center)
+        self.convolvedTemplate  = None
+        self.name               = params['name']
         
     def __repr__(self):
         ''' 
         the string representation of the object
         '''
         return self.name
-    
-    def quickConvolution(self):
+        
+    def quickConvolution(self,parfile,verbose=True,cleanup=True):
         '''
         performs a quick gtsrcmap call on the model
         '''
-        pass
+        if not self.convolvedTemplate is None:
+            print 'Warning, convolved template attribute already set, will overwrite model definition.'
+        templateName = "%s_spatial.fits"%self.name
+        self.spatialModel.save_template(templateName,npix=500)
+        os.environ['SRC_TEMPLATE']=os.path.abspath(templateName)
+        cmd,parDict = par2cmd(parfile,returnDict=True)
+        print cmd
+        pts = subprocess.Popen(cmd,shell=True)
+        pts.communicate()
+        if cleanup:
+            os.remove(templateName)
+            # add more cleanup files.
+        self.convolvedTemplate = parDict['outfile']
+        return self.convolvedTemplate
     
-    def findRadius(self,scaled_srcmap,algorithm='delta'):
+    def findRadius(self,scaled_srcmap,algorithm='delta_average'):
         '''
         depending on the algorithm, return the disk radius that matches best
         '''
-        pass
-
+        def val2str(val): return ("%1.1f"%val).replace(".","d")
+            
+        def delta_average(a,b):
+            avg_a = np.average(a,axis=0)
+            avg_b = np.average(b,axis=0)
+            return np.abs(avg_a - avg_b)
+        # implement other algorithms as we need.
+        
+        if self.convolvedTemplate is None:
+            raise Exception("can't find convolved template. Have you run the convolution yet?")
+        target = np.array(pyfits.getdata(self.convolvedTemplate,"target"),dtype=float)
+        matching = None
+        for i in range(0,10):
+            val = (.1+float(i)/10.)
+            reference = np.array(pyfits.getdata(scaled_srcmap,val2str(val)),dtype=float)
+            fom = algorithm(target,reference)
+            row = np.array([val,fom])
+            if matching is None:
+                matching = row
+            else:
+                matching = np.vstack((matching,row))
+        # best key
+        return val2str(matching.T[0][np.argmin(matching.T[1])])
+    
 def init_models(configfile):
     '''
     the work horse - here all models are instantiated and returned as list
     '''
-    pass
+    modelfile = yaml_load(configfile['models'])
+    models = [CRModel(modelfile[m]) for m in modelfile]
+    return models
