f0dcc72b0a5acb597e7b0e2b16f6984b017bbf38
zimmer <zimmer@fysik.su.se>
2016-01-25 16:38:09 +0100

    fixes.

--- base/CosmicRayPhysics.py
+++ base/CosmicRayPhysics.py
@@ -114,7 +114,7 @@ class CRModel(object):
             e_min, e_max = ebounds['E_MIN'],ebounds['E_MAX']
             energies = np.power(10,np.log10(e_min)+(np.log10(e_max)-np.log10(e_min))/2.)
             e0 = e_min[0]
-            Z_a = np.sum(np.array([scaled_slice(a[i],e,e0) for i,e in enumerate(e_min)]))
+            Z_a = np.sum(np.array([scaled_slice(a[i],e,e0) for i,e in enumerate(e_min)]),axis=0)
             return Z_a
         # implement other algorithms as we need.
         
