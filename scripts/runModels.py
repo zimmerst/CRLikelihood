44991b8996f4bb802489f673c562a1eae2df23a5
zimmer <zimmer@fysik.su.se>
2017-01-16 16:33:14 +0100

    fix.

--- scripts/runModels.py
+++ scripts/runModels.py
@@ -99,7 +99,7 @@ for i,model in enumerate(models_to_test):
         r['ulimits99'][i]  = ProfileLimit( lnlx, lnly).getLimit( 0.01 )
         r['fluxes99'][i]  = ProfileLimit( flux, flnl).getLimit( 0.01 )
         r['ts'][i] = float(2*(p1lnlfn(p1lnlfn.mle()) - p1lnlfn(0)))
-        r['matching_radius'][i]=matching_disk
+        r['matching_radius'][i]=matching_disk if opts.pointlike is None else 0. 
         r['fom']=foms 
     except Exception, message:
         print 'caught exception ',message
